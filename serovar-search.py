356e911996f01796c843eb93a38dba26b2d333a0
CaileanCarter <cailean.carter@quadram.ac.uk>
2021-05-12 17:05:54 +0100

    first commit

--- serovar-search.py
+++ serovar-search.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 from Bio import Entrez
 
-from litsearch import PubMed, ask_email, search
+from litsearch import PubMed, ask_email
 
 
 
@@ -19,6 +19,24 @@ def find_terms(text):
         return n
     except AttributeError:
         return np.NaN
+
+
+def search(df, search: str, top=10) -> pd.DataFrame:
+    """Search for a given serovar"""
+    first_letter, remaining = search[0], search[1:]
+    match = f"[{first_letter.upper()}{first_letter.lower()}]{remaining}"
+    b = df["Serovar"].str.findall(f"{search}")
+    b = b[b.astype(str) != '[]']
+    result = df.loc[b.index]
+    if isinstance(top, int):
+        return result.head(top)
+    else:
+        return result
+
+
+def Serovars(df, top=10):
+    print(df["Serovar"].value_counts()[:top])
+
 
 
 def main(csv_file):
@@ -36,10 +54,10 @@ if __name__ == "__main__":
 
     if argv[2].endswith(".csv"):
         fp = argv[2]
-        skip_arg_two = False
-    else:
-        fp = "escherichi-set.csv"
-        skip_arg_two = True
+        # skip_arg_two = False
+    # else:
+    #     fp = "escherichi-set.csv"
+    #     skip_arg_two = True
 
 
     if argv[1] == "new":
@@ -49,17 +67,21 @@ if __name__ == "__main__":
 
     elif argv[1] == "find":
         df = pd.read_csv(fp, index_col=0)
-        if not skip_arg_two:
-            term = argv[3]
-        else:
-            term = argv[2]
+        # if not skip_arg_two:
+        term = argv[3]
+        # else:
+            # term = argv[2]
         print(search(df, term))
 
 
     elif argv[1] == "summary":
         ask_email()
-        if not skip_arg_two:
-            pmid = argv[3]
-        else:
-            pmid = argv[2]
+        # if not skip_arg_two:
+        pmid = argv[3]
+        # else:
+            # pmid = argv[2]
         PubMed.long_summary(pmid)
+
+    
+    elif argv[1] == "serovars":
+        df = pd.read_csv(fp, index_col=0)
